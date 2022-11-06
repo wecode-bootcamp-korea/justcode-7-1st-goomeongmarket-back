13dfe04c4f0832d52bdce6c9ef68ba8a22b4a140
Kimseonghee <kimsunghee625@gmail.com>
2022-11-06 20:33:25 +0900

    FEAT: create cartList api complete

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -3,8 +3,7 @@ const cartService = require("../services/cartService");
 //메인페이지에서 상품사진에 있는 장바구니 모양 눌렀을때 실행될 api
 const cartUpdate = async (req, res) => {
   try {
-    // const { product_name } = req.body;
-    const product_name = "hihihi";
+    const { product_name } = req.body;
     const REQUIRED_KEYS = {
       product_name,
     };
@@ -29,24 +28,9 @@ const cartUpdate = async (req, res) => {
 
 //페이지상의 장바구니 버튼을 눌렀을 때 해당 유저가 담은 제품 목록을 알려주는 api
 const cartList = async (req, res) => {
-  // console.log("hi");
-  // res.json({ message: "hi" });
   try {
-    const { product_name } = req.body;
-
-    const REQUIRED_KEYS = {
-      product_name,
-    };
-
-    Object.keys(REQUIRED_KEYS).map((key) => {
-      if (!REQUIRED_KEYS[key]) {
-        const error = new Error(`KEY_ERROR: ${key}`);
-        error.statusCode = 400; //잘못된 값이 포함되거나 값이 없을때 400
-        throw error;
-      }
-    });
-    const result = await cartService.cartList(product_name);
-    res.status(200).json({ message: success, data: result });
+    const result = await cartService.cartList();
+    res.status(200).json({ message: "success", data: result });
   } catch (err) {
     console.log(err);
     res.status(err.statusCode).json({ message: err.message });
