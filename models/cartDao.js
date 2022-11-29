7eae5f458fdb89a145127e1a3f2464519d858196
Kimseonghee <kimsunghee625@gmail.com>
2022-11-29 23:24:58 +0900

    REFACTOR: 카트관련 API 리팩토링 및 데이터가 없을때 확인하는 미들웨어 생성

--- models/cartDao.js
+++ models/cartDao.js
@@ -1,20 +1,20 @@
 const myDataSource = require("./index");
 
 const cartUpdate = async (product_id, put_quantity, user_id) => {
-  // const [product] = await myDataSource.query(`
-  //   SELECT id
-  //   FROM products
-  //   WHERE product_id = "${product_id}"
-  // `);
-  // console.log(product_id);
-  await myDataSource.query(`
-  INSERT INTO cart_item (user_id, product_id, put_quantity)
-  VALUES (${user_id}, ${product_id}, ${put_quantity})
-  `);
+  await myDataSource.query(
+    `
+    INSERT INTO 
+      cart_item (user_id, product_id, put_quantity)
+    VALUES 
+      (?, ?, ?)
+  `,
+    [user_id, product_id, put_quantity]
+  );
 };
 
 const cartList = async (user_id) => {
-  let listInfo = await myDataSource.query(`
+  let listInfo = await myDataSource.query(
+    `
       SELECT
       cart_item.user_id,
       JSON_ARRAYAGG(
@@ -29,13 +29,11 @@ const cartList = async (user_id) => {
       FROM cart_item
       JOIN products ON cart_item.product_id = products.id
       JOIN product_images ON cart_item.product_id = product_images.product_id
-      WHERE cart_item.user_id = ${user_id}
+      WHERE cart_item.user_id = ?
       GROUP BY cart_item.user_id
-  `);
-  // console.log(listInfo);
-  // listInfo = [...listInfo].map((item) => {
-  //   return { ...item, products: JSON.parse(item.products) };
-  // });
+    `,
+    [user_id]
+  );
 
   return listInfo;
 };
